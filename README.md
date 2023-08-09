873edd8e0415aa79c70254cc1304064114ac24c8
bayusuryowibowo <bayusuryowibowo@gmail.com>
2023-08-09 12:27:01 +0700

    feat: add protected routes on admin-side

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Target:
 - [x] Lifecycle component menggunakan useEffect
 - [x] Membuat server (json-server)
 - [ ] Membuat react app untuk client-user (Styling)
-- [ ] Membuat react app untuk client-admin (CRUD)
+- [x] Membuat react app untuk client-admin (CRUD)
 - [x] Menentukan Thema Aplikasi
 - [x] Menentukan Design dan pemilihan CSS Framework
 
@@ -45,7 +45,7 @@ Target:
 - [x] Lecture - React-Router
 - [ ] Membuat router user-side (min: home, detail)
 - [x] Membuat router admin-side (min: login, add admin, CRUD main entitas, CRUD another entitas)
-- [ ] Membuat privateRoute untuk admin
+- [x] Membuat privateRoute untuk admin
 
 **Report:**
 
@@ -55,7 +55,7 @@ Target:
 
 Target:
 
-- [ ] Lecture - Redux
+- [x] Lecture - Redux
 - [ ] State vs Global State
 - [ ] Bagaimana cara membuat Store
 - [ ] Bagaimana cara membuat Reducer
--- admin-side/src/router.jsx
+++ admin-side/src/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, redirect } from "react-router-dom";
 import HomePage from "./views/HomePage";
 import RegisterPage from "./views/RegisterPage";
 import LoginPage from "./views/LoginPage";
@@ -6,22 +6,37 @@ import Layout from "./components/Layout";
 
 const router = createBrowserRouter([
   {
-    element: <Layout/>,
+    element: <Layout />,
+    loader: () => {
+      const access_token = localStorage.getItem("access_token");
+      if (!access_token) throw redirect("/login");
+      return null;
+    },
     children: [
       {
         path: "/",
-        element: <HomePage/>,
-      }
-    ]
+        element: <HomePage />,
+      },
+    ],
   },
   {
     path: "/register",
-    element: <RegisterPage/>
+    element: <RegisterPage />,
+    loader: () => {
+      const access_token = localStorage.getItem("access_token");
+      if (access_token) throw redirect("/");
+      return null;
+    },
   },
   {
     path: "/login",
-    element: <LoginPage/>
-  }
+    element: <LoginPage />,
+    loader: () => {
+      const access_token = localStorage.getItem("access_token");
+      if (access_token) throw redirect("/");
+      return null;
+    },
+  },
 ]);
 
 export default router;
