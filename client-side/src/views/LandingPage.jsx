29d352974d63e6d336b6df5605e99bdc2a8f9a5d
bayusuryowibowo <bayusuryowibowo@gmail.com>
2023-08-13 17:44:17 +0700

    style: styling Navbar & Landing Page

--- client-side/src/views/LandingPage.jsx
+++ client-side/src/views/LandingPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProductData } from "../stores/actions/actionCreator";
 import { useNavigate } from "react-router-dom";
+import "./LandingPage.css";
 
 export default function LandingPage() {
   const products = useSelector((state) => state.product.products);
@@ -14,52 +15,57 @@ export default function LandingPage() {
   }, []);
 
   const handleClickDetailProduct = (id) => {
-    navigate(`/detail/${id}`)
-  }
+    navigate(`/detail/${id}`);
+  };
 
   return (
     <>
-      {isLoading ? (
-        <>
-          <div className=" grid grid-cols-5 animate-pulse">
-            <div className="rounded-none shadow-xl lg:rounded-lg lg:block lg:border-4 lg:border-solid border-gray-500">
-              <img className="rounded-none lg:rounded-t-sm lg:block w-full object-cover cursor-pointer" />
-              <div className="p-2 flex flex-col justify-around items-center h-28">
-                <div>
-                  <h5 className="text-xl text-black font-semibold text-center">
-                    Loading...
-                  </h5>
+      <div className=" p-3">
+        {isLoading ? (
+          <>
+            <div className=" grid grid-cols-5 animate-pulse">
+              <div className="rounded-none shadow-xl lg:rounded-lg lg:block lg:border-4 lg:border-solid border-gray-500">
+                <img className="rounded-none lg:rounded-t-sm lg:block w-full object-cover cursor-pointer" />
+                <div className="p-2 flex flex-col justify-around items-center h-28">
+                  <div>
+                    <h5 className="text-xl text-black font-semibold text-center">
+                      Loading...
+                    </h5>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </>
-      ) : (
-        <div className=" grid grid-cols-5">
-          {products.map((product) => (
-            <div
-              key={product.id}
-              className="rounded-none shadow-xl lg:rounded-lg lg:block lg:border-4 lg:border-solid border-gray-500"
-            >
-              <img
-                onClick={() => handleClickDetailProduct(product.id)}
-                src={product.mainImg}
-                className="rounded-none lg:rounded-t-sm lg:block w-full object-cover cursor-pointer"
-              />
-              <div className="p-2 flex flex-col justify-around items-center h-28">
-                <div>
-                  <h5 className="text-xl text-black font-semibold text-center">
+          </>
+        ) : (
+          <div className=" p-3 grid grid-cols-5 gap-y-5">
+            {products.map((product) => (
+              <div
+                key={product.id}
+                className="rounded-none lg:block hover:border-2 hover:shadow-md border-b-2"
+              >
+                <img
+                  onClick={() => handleClickDetailProduct(product.id)}
+                  src={product.mainImg}
+                  className="rounded-none block w-full object-cover cursor-pointer"
+                />
+                <div className="p-2 flex flex-col gap-1 justify-around items-center h-28">
+                  <h5 className=" text-base text-black font-semibold text-center">
                     {product.name}
                   </h5>
-                  <span className=" text-lg text-black font-medium text-center">
-                    $ {product.price}
-                  </span>
+                  <h6 id="price" className=" text-3xl font-bold text-center">
+                    ${product.price}
+                  </h6>
+                </div>
+                <div id="compare" className=" text-center w-full">
+                <button id="button-compare" className=" text-xs font-semibold align-middle transition ease-in-out delay-150 duration-300">
+                  COMPARE
+                </button>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-      )}
+            ))}
+          </div>
+        )}
+      </div>
     </>
   );
 }
