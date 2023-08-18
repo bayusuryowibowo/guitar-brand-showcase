0cbfd5fe649dce104e8528f87eab828ab14c1980
bayusuryowibowo <bayusuryowibowo@gmail.com>
2023-08-18 07:54:02 +0700

    feat: add all reading for public on orchestrator

--- server/orchestrator-express/constants/baseUrl.js
+++ server/orchestrator-express/constants/baseUrl.js
@@ -2,5 +2,15 @@ const USERS_API = "http://localhost:4001";
 const APP_API = "http://localhost:4002";
 
 const APP_PRODUCTS = "app:products";
+const APP_PRODUCT = "app:product";
+const APP_CATEGORIES = "app:categories";
+const APP_CATEGORY = "app:category";
 
-module.exports = { USERS_API, APP_API, APP_PRODUCTS }
+module.exports = {
+  USERS_API,
+  APP_API,
+  APP_PRODUCTS,
+  APP_PRODUCT,
+  APP_CATEGORIES,
+  APP_CATEGORY,
+};
--- server/orchestrator-express/routes/index.js
+++ server/orchestrator-express/routes/index.js
@@ -4,5 +4,8 @@ const Controller = require("../controllers");
 const router = express.Router();
 
 router.get("/pub/products", Controller.readProducts)
+router.get("/pub/products/:id", Controller.readProductById)
+router.get("/pub/categories", Controller.readCategories)
+router.get("/pub/categories/:id", Controller.readCategoryById)
 
 module.exports = router;
